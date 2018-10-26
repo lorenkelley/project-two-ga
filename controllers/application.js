b4a196c1db7e0087fbffe0200b7f10b7afe186e7
CB_ATL_115_Loren_Kelly <cb_atl_115_loren_kelly@MacBook-Air-90.local>
2018-10-26 11:09:58 -0400

    setting up controllers and routes

---
+++ controllers/application.js
@@ -0,0 +1,13 @@
+// 'boilerplate' for application controller
+const applicationController ={
+    index: (req, res) => {
+        res.send('Hey whats up this is app index')
+    }
+}
+
+
+
+
+
+// export this file to the rest of the application
+module.exports = applicationController
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
-var express = require('express');
-var router = express.Router();
+// bring in express
+const express = require('express')
+const router =  express.Router()
+const applicationController = require('../controllers/application')
+const listController = require('../controllers/list')
+const productsController = require('../controllers/products')
+const userController = require('../controllers/user')
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
 
-module.exports = router;
+// GET home page
+router.get('/', applicationController.index)
+
+
+
+
+
+
+
+
+
+
+// export the router so that it can be acessed in other files
+module.exports = router
