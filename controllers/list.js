4b71293176688d12c224212d2a10363d77a59970
CB_ATL_115_Loren_Kelly <cb_atl_115_loren_kelly@MacBook-Air-90.local>
2018-10-27 15:33:47 -0400

    working on list.js in controllers file (part c)

--- controllers/list.js
+++ controllers/list.js
@@ -4,8 +4,29 @@ const List = require('../models/List')
 listController = {
 
     create: (req, res) => {
-     
-    }
+    List.create(req.body).then((newList)=>{
+        res.redirect('/${newList._id}')
+    })
+    },
+
+    index: (req, res) => {
+        List.find().then((list)=>{
+            res.render('list/index', {
+             list: list   
+            })
+        })
+    },
+    show: (req, res) => {
+        List.findById(req.params.id).then((list)=>{
+            res.render('list/show',{
+                list:list
+            })
+        })
+    },
+    
+
+
+
 }
 
 
