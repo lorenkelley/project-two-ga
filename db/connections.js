23d55130dcf46dab2f5e2963473fa7a1b90b53c5
CB_ATL_115_Loren_Kelly <cb_atl_115_loren_kelly@MacBook-Air-90.local>
2018-11-06 13:36:05 -0500

    update .env

--- db/connections.js
+++ db/connections.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/project-two')
-
-mongoose.connection.once('open', () =>{
-    console.log('Mongoose has connected to MongoDB')
+mongoose.connect(process.env.MONGODB_URI).then(() =>{
+    console.log('connected to mongobb')
 })
 
 module.exports = mongoose
